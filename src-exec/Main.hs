8a16e422baf11adfaa92437ce622684b2eecfc0b
stla <laurent_step@yahoo.fr>
2017-12-21 23:28:51 +0100

    first commit

--- src-exec/Main.hs
+++ src-exec/Main.hs
@@ -2,7 +2,7 @@ module Main
   where
 import           Cubature
 
-main :: IO
+main :: IO ()
 main = do
   hcubature <- cubature 'h' fExample 3 [-2,-2,-2] [2,2,2] 1e-4
   pcubature <- cubature 'p' fExample 3 [-2,-2,-2] [2,2,2] 1e-4
